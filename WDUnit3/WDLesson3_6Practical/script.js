ba3ee7ed167410e384599623eaca50be6348e928
Christianv269 <Christianv269@nycstudents.net>
2026-01-16 14:32:29 +0000

    update

--- WDUnit3/WDLesson3_6Practical/script.js
+++ WDUnit3/WDLesson3_6Practical/script.js
@@ -1,5 +1,14 @@
 // Challenge 2:  Create the function for the Average program.  Refer to the resource link in the HTML for assistance.
+function average(){
+    let g1 = parseFloat(document.getElementedById("g1").value);
+    let g2 = parseFloat(document.getElementedById("g2").value);
+    let g3 = parseFloat(document.getElementedById("g3").value);
+    let output = parseFloatdocument.getElementedById("output_average")
 
+
+
+
+}
 
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
 
