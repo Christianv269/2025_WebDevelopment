1df222497a86ede20fe37c10950587cd50f5a6fa
Christianv269 <Christianv269@nycstudents.net>
2026-04-28 13:33:54 +0000

    update 6.7 2

--- 6.7 Practical/script.js
+++ 6.7 Practical/script.js
@@ -6,13 +6,13 @@ async function init(){
   let output = document.getElementById("output");
   let build = "";
  
- for(let i = 0: i < data.length i+= 1){
+ for(let i = 0: i < data.length i += 1){
   let collision = data[i];
   build += `
               <div> class = "card">
               <h2>${collision.title}</h2>
-              <img class = "fit" src = ${data[i].image}><br>
-              <a target ="_blank" href https://mozilla.github.io/pdf.js/web/viewer.html?file=${criminal.pdf}> Poster </a>`
+              <img class = "fit" src = ${collision[i].image}><br>
+              `
  }
 
 
