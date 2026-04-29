a9c90983f34c6e2aee77f70b623e82aeb79ed6e9
Christianv269 <Christianv269@nycstudents.net>
2026-04-29 13:31:57 +0000

    update 6.7 day 3

--- 6.7 Practical/script.js
+++ 6.7 Practical/script.js
@@ -6,12 +6,16 @@ async function init(){
   let output = document.getElementById("output");
   let build = "";
  
- for(let i = 0: i < data.length i += 1){
+ for(let i = 0; i < data.length; i += 1){
   let collision = data[i];
   build += `
-              <div> class = "card">
-              <h2>${collision.title}</h2>
-              <img class = "fit" src = ${collision[i].image}><br>
+              <div> class = "fitted card">
+              <h2>${collision.crash_date}</h2>
+              <h2>${collision.crash_time}</h2>
+              <h2>${collision.borough}</h2>
+              <h2>${collision.zip_code}</h2>
+              </div>
+            
               `
  }
 
